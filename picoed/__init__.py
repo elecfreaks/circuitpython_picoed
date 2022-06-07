9eb48f7d4b3f948e7af3afe1541b7a03ffd548e0
Xu Hao <xuhow@outlook.com>
2022-06-07 11:21:00 +0800

    Compatible with v1 and v2

--- picoed/__init__.py
+++ picoed/__init__.py
@@ -18,7 +18,14 @@ __version__ = "0.1.0"
 __repo__ = "https://github.com/elecfreaks/circuitpython_picoed.git"
 
 i2c = busio.I2C(board.SCL, board.SDA)
-display = Display(i2c)
+
+try:
+    # For Pico:ed V2
+    internal_i2c = busio.I2C(board.I2C0_SCL, board.I2C0_SDA)
+    display = Display(internal_i2c)
+except RuntimeError:
+    # For Pico:ed V1
+    display = Display(i2c)
 
 button_a = Button(board.BUTTON_A)
 button_b = Button(board.BUTTON_B)
