745e33bb169c1345b87a99c27d4b6d9cf05076ac
Xu Hao <xuhow@outlook.com>
2022-06-28 14:16:25 +0800

    Add was_pressed method of class Button

--- picoed/button.py
+++ picoed/button.py
@@ -7,7 +7,17 @@ class Button():
 
     def __init__(self, pin: microcontroller.Pin):
         self._pin = digitalio.DigitalInOut(pin)
+        self._last_pressed = False
 
     def is_pressed(self):
         """Returns True when the key is pressed"""
         return not self._pin.value
+
+    def was_pressed(self):
+        """Returns True when the key was pressed"""
+        if not self._pin.value and not self._last_pressed:
+            self._last_pressed = True
+            return True
+        if self._pin.value:
+            self._last_pressed = False
+        return False
